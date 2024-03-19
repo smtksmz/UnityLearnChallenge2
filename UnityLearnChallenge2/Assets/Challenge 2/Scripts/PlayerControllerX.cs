b9722043656459e0961a37dd8a73e71407e9d1f8
Samet Kosmaz <ksmzsmt@gmail.com>
2024-03-20 01:36:09 +0300

    bug fix

--- UnityLearnChallenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ UnityLearnChallenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private bool coolDown=false;
+    public float coolDownDelay = 1.5f;
+    public float coolDownTInterval = 2.0f;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !coolDown)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            coolDown = true;
+            InvokeRepeating("CoolDown", coolDownDelay, coolDownTInterval);
         }
+    }
+
+    void CoolDown()
+    {
+        coolDown = false;
     }
 }
--- UnityLearnChallenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ UnityLearnChallenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,8 +10,8 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float startDelay = 3.0f;
+    private float spawnInterval =5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +24,10 @@ public class SpawnManagerX : MonoBehaviour
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int ballIndex=Random.Range(0, ballPrefabs.Length);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
